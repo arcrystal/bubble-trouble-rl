f76d77230126ead2df7af28ddf8e7c504f37864a
arcrystal <acrystal@u.rochester.edu>
2022-10-15 15:07:36 -0400

    update ball sprites

--- ball.py
+++ ball.py
@@ -7,11 +7,11 @@ class Ball(pygame.sprite.Sprite):
     """
     DISPLAY_WIDTH = 890
     DISPLAY_HEIGHT = DISPLAY_WIDTH / 1.8737
-    XSPEED = DISPLAY_WIDTH / 100 # FPS, TIMESTEP
+    XSPEED = DISPLAY_WIDTH / 75
     #SPEED = [50, 65, 75, 85, 90, 95, 100]
 
     # Ball size
-    bsize = [
+    bsizes = [
         DISPLAY_WIDTH / 50.7273, # 55=b1
         DISPLAY_WIDTH / 29.3684, # 95=b2
         DISPLAY_WIDTH / 15.7627, # 177=b3
@@ -20,16 +20,14 @@ class Ball(pygame.sprite.Sprite):
         DISPLAY_WIDTH / 6.7718] # 412=b6
 
     SPRITES = {
-        0:pygame.image.load("Sprites/ball1_sm.png"),
-        1:pygame.image.load("Sprites/ball2_sm.png"),
-        2:pygame.image.load("Sprites/ball3_sm.png"),
-        3:pygame.image.load("Sprites/ball4_sm.png"),
-        4:pygame.image.load("Sprites/ball5_sm.png"),
-        5:pygame.image.load("Sprites/ball6_sm.png"),
-        6:pygame.image.load("Sprites/ball7_sm.png")}
+        'yellow':pygame.image.load("Sprites/ball_yellow.png"),
+        'red':pygame.image.load("Sprites/ball_red.png"),
+        'blue':pygame.image.load("Sprites/ball_blue.png"),
+        'purple':pygame.image.load("Sprites/ball_purple.png"),
+        'green':pygame.image.load("Sprites/ball_green.png"),
+        'orange':pygame.image.load("Sprites/ball_orange.png"),
+        'pink':pygame.image.load("Sprites/ball_pink.png")}
 
-    for i in range(6):
-        SPRITES[i] = pygame.transform.scale(SPRITES[i], (bsize[i], bsize[i]))
 
     # Ball bounce height (floor to bottom of ball)
     # [150.855, 311.322, 381.988, 458.35, 530.974]
@@ -42,21 +40,30 @@ class Ball(pygame.sprite.Sprite):
         DISPLAY_HEIGHT * 0.6803
     ] 
     # values will be multiplied by t=0.1 divided by FPS=52
-    SPEED = [sqrt(b*25) for b in bbounce]
+    SPEED = [sqrt(b*35) for b in bbounce]
     # Multiplied by t = 0.1
-    Y_ACC = DISPLAY_WIDTH / 64 # 13.91
+    Y_ACC = DISPLAY_WIDTH / 42.657 # 13.91
 
-    def __init__(self, x, y, xspeed, yspeed, xacceleration, ballsize):
+    def __init__(self, x, y, xspeed, yspeed, xacceleration, ballsize, color):
         assert ballsize < 5
         super().__init__() # equivalent to pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
-        self.xspeed = xspeed
+        if type(xspeed) in [float, int]:
+            self.xspeed = xspeed
+        elif xspeed == 'left':
+            self.xspeed = -Ball.XSPEED
+        elif xspeed == 'right':
+            self.xspeed = Ball.XSPEED
+        else:
+            self.xspeed = 0
+
         self.yspeed = yspeed
         self.xacceleration = xacceleration
         self.yacceleration = Ball.Y_ACC
         self.ballsize = ballsize
-        self.image = Ball.SPRITES[ballsize]
+        self.color = color
+        self.image = pygame.transform.scale(Ball.SPRITES[color], (Ball.bsizes[ballsize], Ball.bsizes[ballsize]))
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
 
@@ -119,6 +126,6 @@ class Ball(pygame.sprite.Sprite):
             return None
         else:
             newYspeed = -25
-            return (Ball(self.x-10, self.y, -Ball.XSPEED, newYspeed, 0, self.ballsize-1),
-                    Ball(self.x+10, self.y, Ball.XSPEED, newYspeed, 0, self.ballsize-1))
+            return (Ball(self.x-10, self.y, -Ball.XSPEED, newYspeed, 0, self.ballsize-1, self.color),
+                    Ball(self.x+10, self.y, Ball.XSPEED, newYspeed, 0, self.ballsize-1, self.color))
 
