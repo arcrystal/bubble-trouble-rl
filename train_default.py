2e8c6ae2cbf920b36b6f257d8a0e6e6362b59b9c
arcrystal <acrystal@u.rochester.edu>
2023-12-22 18:29:03 -0500

    remove unused methods

--- train_default.py
+++ train_default.py
@@ -143,45 +143,13 @@ def simulate_model(ckpt):
         print(f"Cumulative reward: {cumulative_reward}")
 
 
-
-
-def get_stats(ckpt):
-    algo = Algorithm.from_checkpoint(ckpt)
-    policy = algo.get_policy()
-    # <ray.rllib.policy.eager_tf_policy.PPOTFPolicy_eager object at 0x7fd020165470>
-
-    # Run a forward pass to get model output logits. Note that complex observations
-    # must be preprocessed as in the above code block.
-    logits, _ = policy.model({"obs": np.array([[0.1, 0.2, 0.3, 0.4]])})
-    # (<tf.Tensor: id=1274, shape=(1, 2), dtype=float32, numpy=...>, [])
-
-    # Compute action distribution given logits
-    policy.dist_class
-    # <class_object 'ray.rllib.models.tf.tf_action_dist.Categorical'>
-    dist = policy.dist_class(logits, policy.model)
-    # <ray.rllib.models.tf.tf_action_dist.Categorical object at 0x7fd02301d710>
-
-    # Query the distribution for samples, sample logps
-    dist.sample()
-    # <tf.Tensor: id=661, shape=(1,), dtype=int64, numpy=..>
-    dist.logp([1])
-    # <tf.Tensor: id=1298, shape=(1,), dtype=float32, numpy=...>
-
-    # Get the estimated values for the most recent forward pass
-    policy.model.value_function()
-    # <tf.Tensor: id=670, shape=(1,), dtype=float32, numpy=...>
-
-    print(policy.model.base_model.summary())
-
-
 if __name__ == "__main__":
     project_dir = "/Users/acrystal/Desktop/Coding/bubble-trouble-rl/"
     ckpt = project_dir + "Results/lstm_ppo_v9/checkpoint_000025"
-    # args = get_args()
-    # ray.init()
-    # config = tune_model(args.tune_iters)
-    # ppo_algo = train_model(args.train_iters, config, ckpt=ckpt)
-    # plot()
-    #get_stats(ckpt)
+    args = get_args()
+    ray.init()
+    config = tune_model(args.tune_iters)
+    ppo_algo = train_model(args.train_iters, config, ckpt=ckpt)
+    plot()
     simulate_model(ckpt)
-    # ray.shutdown()
+    ray.shutdown()
